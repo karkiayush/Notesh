c79772bb048088a161190d88a02bd55a54642562
karkiayush <karkiayush619@gmail.com>
2025-01-08 19:02:05 +0545

    change app title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Notesh',
       theme: ThemeData(
         textTheme: GoogleFonts.recursiveTextTheme(Theme.of(context).textTheme),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
